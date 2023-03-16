bf7044d2228bc0d819edb975d2d989f1552de053
ivy-515 <ivy.am515@gmail.com>
2023-03-16 16:54:05 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,11 @@
 
 int main()
 {
-	/* writig a string using puts */
+	/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts (str);
 	return 0;
