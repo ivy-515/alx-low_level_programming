e876f3c61dce074b631103b3c9b01e4039cea555
ivy-515 <ivy.am515@gmail.com>
2023-03-17 13:10:33 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,10 +12,10 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	lastdigit = n % 10;
-	if (lastdigit > 0)
+	
+	if (n % 10 > 0)
 		printf("Last digit of %d is and is greater than 5\n", n);
-	else if (lastgigit == 0)
+	else if (n % 10 == 0)
 		printf("Last digit of %d is and is 0\n", n);
 	else
 		printf("Last digit of %d is and is less than 6 and not 0\n", n);
