5ca29f4ed6f8db561c66067d95b0d5f5094ce973
ivy-515 <ivy.am515@gmail.com>
2023-03-17 14:44:13 -0700

    pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'd'; i++ && i = 'f'; i <= 'p'; i++ && i = 'r'; i<= 'z', r++);
+	{
+		putchar (i);
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
