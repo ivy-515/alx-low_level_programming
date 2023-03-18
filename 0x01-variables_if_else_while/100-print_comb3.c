075b463118470b5b01ef5a21a7fd8305d4ccc413
ivy-515 <ivy.am515@gmail.com>
2023-03-17 17:34:30 -0700

    The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,21 +8,21 @@
 
 int main(void)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i <= 8; i++)
-
+	{
 		for (j = i + 1; j <= 9; j++)
 		
 			putchar (i + '0');
 			putchar (j + '0');
 
-		if (i == 8 && j == 9)
-			continue;
+			if (i == 8 && j == 9)
+				continue;
 
-		putchar (',');
-		putchar (' ');
+			putchar (',');
+			putchar (' ');
+	}
 
 	return (0);
 }
