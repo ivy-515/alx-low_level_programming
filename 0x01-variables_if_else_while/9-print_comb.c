c313ba2a09e4aa4b030197a4f82ac598853b9ec8
ivy-515 <ivy.am515@gmail.com>
2023-03-17 17:06:33 -0700

    unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,11 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar ('0' + i);
-		if (i != 9)
+		if (i == 9)
+			continue;
 
-			putchar (',');
-			putchar (' ');
+		putchar (',');
+		putchar (' ');
 	}
 
 	putchar ('\n');
