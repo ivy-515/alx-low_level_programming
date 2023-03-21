884ca898d90d387142a68712909a86a05192b29d
ivy-515 <ivy.am515@gmail.com>
2023-03-21 12:22:09 -0700

    0putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
-* Main - check description
-* Description : It prints the word _putchar, followed by a new line.
-* Return : 0.
-*/
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return: 0.
+ */
 
 int main(void)
 {
