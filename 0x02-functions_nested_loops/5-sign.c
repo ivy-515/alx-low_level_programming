66ffec0efdd5f5562414d8763e4045dca43c3a06
ivy-515 <ivy.am515@gmail.com>
2023-03-21 15:12:22 -0700

    cor 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,16 +14,22 @@ int print_sign(int n)
 	int value;
 
 	if (n < 0)
+	{
 		value = -1;
 		_putchar('-');
 
-	else if (n = 0)
-		value = 0;
-		_putchar('0');
-
-	else
+	}
+	else if (n > 0)
+	{
 		value = 1;
 		_putchar('+');
+	}
+
+	else
+	{
+		value = 0;
+		_putchar('0');
+	}
 
 	return (value);
 }
