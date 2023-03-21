e8f003624c5dad78310dfbd78bcbcf9669d49050
ivy-515 <ivy.am515@gmail.com>
2023-03-21 12:15:05 -0700

    0putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,12 @@
 #ifndef main_h_
 #define main_h_
 
+/*
+ * File: main.h
+ * Description: Header file containing declarations for all functions
+ * 		used in the 0x02-functions_nested_loops directory.
+ */
+
 int _putchar(char c);
 
 #endif /* _putchar */
