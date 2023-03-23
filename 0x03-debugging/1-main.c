f1cbc9317c2e1ee56d727da2e771b294e6aa0889
ivy-515 <ivy.am515@gmail.com>
2023-03-23 09:37:43 -0700

    cor task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,11 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
-	} /* infinite loop */
+	/** while (i < 10)
+	 *{
+	 *	putchar(i);
+	 *}
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
