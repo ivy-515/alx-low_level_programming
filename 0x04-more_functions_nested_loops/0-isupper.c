00715fc359026d1c62b9050a5572044173c7e84d
ivy-515 <ivy.am515@gmail.com>
2023-03-23 15:46:13 -0700

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - check main.
+ * Description : checks for uppercase character.
+ * @c : character
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+	int value;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		value = 0;
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		value = 1;
+	}
+
+	return (value);
+}
