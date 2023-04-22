24fac2b6206bcff9841441d5b81cd57aee49a49f
ivy-515 <ivy.am515@gmail.com>
2023-04-22 03:46:46 -0700

    check 17

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
 
 /**
  * get_op_func - a function that selects the correct
@@ -18,12 +17,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (i < 5)
 	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 		i++;
 	}
 
-	return (ops[i].f);
+	return (0);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,32 +8,25 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int num1, num2, res;
 	int (*opp)(int, int);
-	char *operator;
+	int res;
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
 
-	operator = argv[2];
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[3]);
-
-	opp = get_op_func(operator);
+	opp = get_op_func(argv[2]);
 
 	if (!opp)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	res = opp(num1, num2);
-
+	res = opp(atoi(argv[1]), atoi(argv[3]));
 	printf("%d\n", res);
 
 	return (0);
