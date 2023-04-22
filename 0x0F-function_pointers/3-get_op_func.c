fb1986c0be4953f62a59f336ac104f9419b34970
ivy-515 <ivy.am515@gmail.com>
2023-04-22 03:35:22 -0700

    check 17

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,15 +18,12 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-
 	int i = 0;
 
-	while (i < 5)
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 	{
-		if (*(ops[i].op) == *s)
-			return (ops[i].f);
 		i++;
 	}
 
-	return (NULL);
+	return (ops[i].f);
 }
